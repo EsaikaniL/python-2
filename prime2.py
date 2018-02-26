ce978ed12d76fba6c229f8dad9d8bcf730456613
Hemkalapoudel <36299235+Hemkalapoudel@users.noreply.github.com>
2018-02-26 17:17:57 +0530

    prime2
    getting prime number between two interval

--- prime2.py
+++ prime2.py
@@ -1,7 +1,6 @@
 lower = int(input("Enter lower range: "))
 upper = int(input("Enter upper range: "))
- 
-for num in range(lower,upper + 1):
+ for num in range(lower,upper + 1):
    # prime numbers are greater than 1
    if num > 1:
        for i in range(2,num):
