10f3cf02e93898b172bea54e3e435fff51b5c083
Hemkalapoudel <36299235+Hemkalapoudel@users.noreply.github.com>
2018-03-12 17:56:52 +0530

    index
    print index of each element

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-n=int(input("Enter the size"))
-l=[]
+
+n = int(input("Enter the size"))
+l = []
 for i in range(0,n):
-  b=int(input())
+  b = int(input())
   l.append(b)
 for x in l:
     print(x,l.index(x))
